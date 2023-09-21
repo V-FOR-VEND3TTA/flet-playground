d57a261dffeebf8e47e0e4279a0c4ecce45d8211
George Hlongwane <georgehlongwane8@gmail.com>
2023-09-21 10:55:58 +0200

    Create my first Flet application with the sample code

---
+++ counter.py
@@ -0,0 +1,31 @@
+import flet as ft
+
+def main(page: ft.Page):
+    page.title = "Counter App"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+
+    txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
+
+    def minus_click(e):
+        txt_number.value = str(int(txt_number.value) - 1)
+        page.update()
+
+    def plus_click(e):
+        txt_number.value = str(int(txt_number.value) + 1)
+        page.update()
+
+    page.add(
+        ft.Row(
+            [
+                ft.IconButton(ft.icons.REMOVE, on_click=minus_click),
+                txt_number,
+                ft.IconButton(ft.icons.ADD, on_click=plus_click),
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+        )
+    )
+
+# Run as Desktop application
+ft.app(target=main)
+
+# Run as a browser app
